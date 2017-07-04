2a83d5e589f2909a3f0b066fe23eb355f509ea10
hdpdrupal <degonzalez21@outlook.com>
2017-07-04 11:08:26 -0600

    Delete instalardrush.sh

--- instalardrush.sh
+++
@@ -1,8 +0,0 @@
-
-cd ~/ 
-wget http://ftp.drupal.org/files/projects/drush-7.x-5.1.tar.gz
-tar -xzf drush-7.x-5.1.tar.gz
-cd ~/drush c
-hmod -R 0777 drush
-cd /var/www/html/drupal 
-chmod 0777 php.ini
