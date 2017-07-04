77bb5baed80293847785a0d39837ba6e1a1f6e07
hdpdrupal <degonzalez21@outlook.com>
2017-07-04 00:32:52 -0600

    Update instalardrupal.sh

--- drupal/instalardrupal.sh
+++ drupal/instalardrupal.sh
@@ -2,6 +2,6 @@
 
 cd /var/www/html/drupal
 
-drush site-install --db-url=mysql://root:4321@localhost/drupal --site-name="JCCOMPUTADORAS" --account-name="cadenas" --account-pass="4321" --locale=es standard
-"y"
+drush site-install -y --db-url=mysql://root:4321@localhost/drupal --site-name="JCCOMPUTADORAS" --account-name="cadenas" --account-pass="4321" --locale=es standard
+
 
