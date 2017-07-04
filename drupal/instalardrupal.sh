f80d0c4e3888a9947efc012b692710038bf53acf
hdpdrupal <degonzalez21@outlook.com>
2017-07-03 22:15:55 -0600

    Add files via upload

---
+++ drupal/instalardrupal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /var/www/html/drupal
+
+drush site-install --db-url=mysql://root:4321@localhost/drupal --site-name="JCCOMPUTADORAS" --account-name="cadenas" --account-pass="4321" --locale=es standard
+
+
