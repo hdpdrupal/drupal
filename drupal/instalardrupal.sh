d069920432a56462f6b23e291e98b6f332ead97a
hdpdrupal <degonzalez21@outlook.com>
2017-07-03 22:34:39 -0600

    Delete instalardrupal.sh

--- drupal/instalardrupal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-
-cd /var/www/html/drupal
-
-drush site-install --db-url=mysql://root:4321@localhost/drupal --site-name="JCCOMPUTADORAS" --account-name="cadenas" --account-pass="4321" --locale=es standard
-
-
